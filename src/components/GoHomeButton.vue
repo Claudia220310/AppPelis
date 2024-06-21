ea2abbcec5295ca5c3f8459cc391e7fc828320d0
Claudia220310 <norena1466@gmail.com>
2024-06-20 23:24:49 -0500

    Se creó componente para el buton de inicio

---
+++ src/components/GoHomeButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <ion-card>
+      <ion-button @click="goHome" color="primary">IR AL INICIO</ion-button>
+    </ion-card>
+  </template>
+  
+  <script lang="ts">
+  import { IonButton, IonCard } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  export default defineComponent({
+    components: {
+      IonButton,
+      IonCard,
+    },
+    setup() {
+      const router = useRouter();
+  
+      const goHome = () => {
+        router.push({ name: 'HomePage' });
+      };
+  
+      return {
+        goHome,
+      };
+    },
+  });
+  </script>
+  
+  <style scoped>
+  /* Estilos adicionales para el botón si son necesarios */
+  ion-button {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+  }
+  </style>
+  
