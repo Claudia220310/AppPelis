52c0b2f128d6fe21a12c4aa2875f4ee408c1a0bd
Claudia220310 <norena1466@gmail.com>
2024-06-21 01:38:43 -0500

    Se creó un componente para manejar los botones de vista principal

---
+++ src/components/FilterButtons.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="filters">
+      <ion-button @click="filterByCategory('now_playing')">Cartelera</ion-button>
+      <ion-button @click="filterByCategory('top_rated')">Mejor Calificadas</ion-button>
+      <ion-button @click="filterByCategory('popular')">Populares</ion-button>
+    </div>
+  </template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { IonButton } from '@ionic/vue';
+  
+  export default defineComponent({
+    components: {
+      IonButton
+    },
+    emits: ['filter-category'],
+    methods: {
+      filterByCategory(category: string) {
+        this.$emit('filter-category', category);
+      }
+    }
+  });
+</script>
+  
+<style scoped>
+  .filters {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+    padding: 1rem;
+    margin-top: 2rem;
+  }
+</style>
+  
